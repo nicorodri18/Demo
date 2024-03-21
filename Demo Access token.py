9c0ccde8cbeb78740e97c9802b4e3c0813c3cdca
nicorodri18 <100327214+nicorodri18@users.noreply.github.com>
2024-03-21 06:38:13 -0500

    Version Final Access Token
    Demo de autenticación con GUI de un programa que simula el uso de Access Token con JWT para su funcionamiento.

--- Demo Access token.py
+++ Demo Access token.py
@@ -6,12 +6,12 @@ import tkinter as tk
 from tkinter import messagebox
 import pyperclip
 
-# Clave secreta para firmar y verificar los tokens JWT
+
 clave_secreta = secrets.token_hex(16)
-# Base de datos simulada de usuarios
+
 base_de_datos_usuarios = {}
 
-# Función para simular la autenticación del usuario
+
 def autenticar_usuario(nombre_usuario, contrasena):
     if nombre_usuario in base_de_datos_usuarios:
         hashed_password, salt = base_de_datos_usuarios[nombre_usuario]
@@ -20,31 +20,31 @@ def autenticar_usuario(nombre_usuario, contrasena):
             return True
     return False
 
-# Función para registrar un nuevo usuario
+
 def registrar_usuario(nombre_usuario, contrasena):
     if nombre_usuario in base_de_datos_usuarios:
-        return False  # El nombre de usuario ya existe
+        return False  
     else:
         salt = secrets.token_bytes(16)
         hashed_password = hashlib.pbkdf2_hmac('sha256', contrasena.encode('utf-8'), salt, 100000)
         base_de_datos_usuarios[nombre_usuario] = (hashed_password, salt)
         return True
 
-# Función para generar un token de acceso JWT después de la autenticación exitosa
+
 def generar_token(nombre_usuario, rol='usuario'):
-    # Crear el payload para el token JWT
+   
     payload = {
         'nombre_usuario': nombre_usuario,
         'rol': rol,
-        'exp': datetime.utcnow() + timedelta(minutes=30)  # Token de acceso válido por 30 minutos
+        'exp': datetime.utcnow() + timedelta(minutes=30) 
     }
 
-    # Generar el token JWT
+
     token = jwt.encode(payload, clave_secreta, algorithm='HS256')
 
     return token
 
-# Función para verificar el token de acceso
+
 def verificar_token(token):
     try:
         payload = jwt.decode(token, clave_secreta, algorithms=['HS256'])
@@ -54,7 +54,7 @@ def verificar_token(token):
     except jwt.InvalidTokenError:
         return {'mensaje': 'Token de acceso inválido'}
 
-# Función para manejar el evento de inicio de sesión en la GUI
+
 def iniciar_sesion():
     nombre_usuario = entry_usuario.get()
     contrasena = entry_contrasena.get()
@@ -66,7 +66,6 @@ def iniciar_sesion():
         messagebox.showerror('Error de autenticación', 'Autenticación fallida. Credenciales inválidas.')
         ventana.destroy()
 
-# Función para abrir la ventana de verificación de token en la GUI
 def ventana_verificacion(token):
     ventana_verificacion = tk.Toplevel()
     ventana_verificacion.title('Verificar Token de Acceso')
@@ -94,7 +93,6 @@ def verificar_token_gui(token):
     else:
         messagebox.showinfo('Verificación exitosa', 'Token de acceso verificado correctamente.')
 
-# Función para manejar el evento de registro en la GUI
 def registrar():
     nuevo_usuario = entry_usuario.get()
     nueva_contrasena = entry_contrasena.get()
@@ -103,11 +101,11 @@ def registrar():
     else:
         messagebox.showerror('Error de registro', f'Error: El usuario {nuevo_usuario} ya existe.')
 
-# Crear la ventana principal de la GUI
+
 ventana = tk.Tk()
 ventana.title('Demo de autenticación y autorización')
 
-# Crear etiquetas y campos de entrada para usuario y contraseña
+
 tk.Label(ventana, text='Nombre de usuario:').pack()
 entry_usuario = tk.Entry(ventana)
 entry_usuario.pack()
@@ -116,12 +114,12 @@ tk.Label(ventana, text='Contraseña:').pack()
 entry_contrasena = tk.Entry(ventana, show='*')
 entry_contrasena.pack()
 
-# Botones para iniciar sesión y registrarse
+
 btn_iniciar_sesion = tk.Button(ventana, text='Iniciar Sesión', command=iniciar_sesion)
 btn_iniciar_sesion.pack(pady=10)
 
 btn_registrar = tk.Button(ventana, text='Registrarse', command=registrar)
 btn_registrar.pack(pady=5)
 
-# Ejecutar la interfaz gráfica
+
 ventana.mainloop()
